06bbb980140c863ef7b4f0e3be1e1d15aed64b9d
qwr133 <121946724+qwr133@users.noreply.github.com>
2023-04-25 19:41:29 +0900

    전통적인 JDBC API - 조회, 삽입, 수정, 삭제

---
+++ src/main/java/com/spring/mvc/config/DatabaseConfig.java
@@ -0,0 +1,35 @@
+package com.spring.mvc.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration  //설정파일 선언
+public class DatabaseConfig {
+
+
+    //데이터 소스 설정: 데이터베이스 연결 정보
+    // url: dbms가 설치된 경로
+    //username : db 계정명
+    //password : db password
+    //driver clasee : DBMS마다 설치한 커넥터 드라이버
+
+    //커넥션 풀 설정
+    // db 접속시 사용하는 리소스를 관리하는 프로그램
+
+//    @Bean
+//    public DataSource dataSource(){
+//        //히카리 설정
+//        HikariConfig config = new HikariConfig();
+//        config.setUsername("root");
+//        config.setPassword("1234");
+//        config.setJdbcUrl("jdbc:mariadb://localhost:3306/spring");
+//        config.setDriverClassName("org.mariadb.jdbc.Driver");
+//
+//        return new HikariDataSource(config);
+//
+//    }
+}
